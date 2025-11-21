5506a0f7cc44c67f23e1e0b478ceb1631921ae54
IMDAD1306 <bouraimasamory@gmail.com>
2025-11-21 15:05:19 +0100

    Add files via upload

---
+++ projets.php
@@ -0,0 +1,65 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+  <meta charset="UTF-8">
+  <title>Projets - Portfolio Imdad BOURAIMA</title>
+<link rel="stylesheet" href="style/style_projets.css">
+</head>
+
+<body>
+
+      <?php require_once 'header.php'; ?>
+
+  <section id="projets" class="section">
+    <h2>Mes projets</h2>
+
+    <ol class="liste-projets">
+
+      <!-- Projet 1 -->
+      <li class="projet">
+        <h3>LegalHelp</h3>
+        <p>Site web permettant de faciliter la mise en relation entre clients et avocats.</p>
+        <a href="#" class="btn" target="_blank">Voir le code</a>
+        <a href="#" class="btn" target="_blank">Voir la présentation</a>
+      </li>
+
+      <!-- Projet 2 -->
+      <li class="projet">
+        <h3>Shopify – Boutique Bijoux de Luxe</h3>
+        <p>Site e-commerce pour vendre des bijoux de luxe avec Shopify.</p>
+        <a href="#" class="btn" target="_blank">Voir le code</a>
+        <a href="#" class="btn" target="_blank">Voir la présentation</a>
+      </li>
+
+      <!-- Projet 3 -->
+      <li class="projet">
+        <h3>Jeu “SNAKE”</h3>
+        <p>Jeu développé en langage C inspiré du jeu Snake.</p>
+        <a href="#" class="btn" target="_blank">Voir le code</a>
+        <a href="#" class="btn" target="_blank">Voir la présentation</a>
+      </li>
+
+      <!-- Projet 4 -->
+      <li class="projet">
+        <h3>ASIH</h3>
+        <p>Site web inspiré d’Uber pour la réservation et gestion de services de transport.</p>
+        <a href="#" class="btn" target="_blank">Voir le code</a>
+        <a href="#" class="btn" target="_blank">Voir la présentation</a>
+      </li>
+
+      <!-- Projet 5 -->
+      <li class="projet">
+        <h3>JEU "Mot mele"</h3>
+        <p>jeu inspire du jeu de mot mele sur mobile retrouver des mots dans un melane de lettre </p>
+        <a href="#" class="btn" target="_blank">Voir le code</a>
+        <a href="#" class="btn" target="_blank">Voir la présentation</a>
+      </li>
+
+
+    </ol>
+  </section>
+
+  <?php require_once('footer.php'); ?>
+
+</body>
+</html>
